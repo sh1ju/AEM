03834793d3ff33252b6fdf5658304dfd42cfdcc3
Yvonne Yew <yyew@apdgroup.com>
2018-02-06 23:10:40 +0800

    Sprint 1 cleanup

---
+++ core/src/main/java/com/taylorsuniversity/core/beans/TextListColumnsBean.java
@@ -0,0 +1,28 @@
+package com.taylorsuniversity.core.beans;
+
+import java.util.List;
+
+public class TextListColumnsBean {	
+	private List<TouchMultiCompositeFieldBean> column1;	
+	private List<TouchMultiCompositeFieldBean> column2;
+	private List<TouchMultiCompositeFieldBean> column3;
+
+	public List<TouchMultiCompositeFieldBean> getColumn1() {
+		return column1;
+	}
+	public void setColumn1(List<TouchMultiCompositeFieldBean> column1) {
+		this.column1 = column1;
+	}
+	public List<TouchMultiCompositeFieldBean> getColumn2() {
+		return column2;
+	}
+	public void setColumn2(List<TouchMultiCompositeFieldBean> column2) {
+		this.column2 = column2;
+	}
+	public List<TouchMultiCompositeFieldBean> getColumn3() {
+		return column3;
+	}
+	public void setColumn3(List<TouchMultiCompositeFieldBean> column3) {
+		this.column3 = column3;
+	}
+}
