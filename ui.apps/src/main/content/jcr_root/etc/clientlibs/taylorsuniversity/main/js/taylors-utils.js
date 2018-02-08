e38d9102e784005ad20588391075a986dfebaa53
bsinghal <bsinghal@bsinghal-WX-1.corp.adobe.com>
2018-02-08 14:51:25 +0530

    Changes for the Application From component

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/main/js/taylors-utils.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/main/js/taylors-utils.js
@@ -1,7 +1,13 @@
-$( "#appointment-button" ).click(function(appointmentPath) {
-    console.log("Appointment Path is: " + appointmentPath);
-});
-
-$( "#livechat-button" ).click(function(livechatPath) {
-    console.log("Live Chat Path is: " + livechatPath);
+$('input[name="email"]').on('change', function() {    
+	var input=$(this);
+	var pattern = /^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+   	//return pattern.test(input.val());
+   	var is_email=pattern.test(input.val());
+   	console.log(is_email);
+   	if(is_email) {
+   		input.removeClass("invalid").addClass("valid");
+   	}
+	else {
+		input.removeClass("valid").addClass("invalid");
+	}
 });
