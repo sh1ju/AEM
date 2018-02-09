b3f82b7b33527bed6bc675e76da427291a4cd910
bsinghal <bsinghal@bsinghal-WX-1.corp.adobe.com>
2018-02-09 22:58:33 +0530

    Changes to make the pathfield un-editable in header & footer on click of 'Add Field'

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/dialogvalidation/js/readonlypathbrowser.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/dialogvalidation/js/readonlypathbrowser.js
@@ -7,11 +7,13 @@ $(document).on("dialog-ready", function() {
     pathReadonly();
 });
 
-$(document).on("click", ".coral-Button", function (e) {    
-    var pathReadonly = function() {        
-    	$(".path-readonly").find(".coral-Textfield").each(function() {                
-            $(this).attr("readonly", "readonly");            
-        });    
-    }    
-    pathReadonly();
+$(document).on("dialog-ready", function () {
+	$(".coral-Button").click(function() {
+		var field = $(this).parent();    
+        setTimeout(function() {
+            field.find(".coral-Textfield").each(function() {
+                $(this).attr("readonly", "readonly");
+            });
+        }, 250);
+	});
 });
