654586904ebc6e6c3b5d71f5f7618e61166402f0
ADOBENET\prajput <prajput@adobe.com>
2018-02-09 14:08:16 +0530

    Code Commit for School Themes

---
+++ core/src/main/java/com/taylorsuniversity/models/SchoolThemesModel.java
@@ -0,0 +1,80 @@
+/* Copyright Taylors University */
+
+package com.taylorsuniversity.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+
+import com.google.gson.Gson;
+import com.taylorsuniversity.models.bean.SchoolThemesModelBean;
+
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class is used to retrieve the properties configured
+ * in the school themes component.
+ */
+@Model(adaptables = SlingHttpServletRequest.class)
+public final class SchoolThemesModel {
+    /**
+     * The Logger
+     */
+    private Logger logger = LoggerFactory.getLogger(SchoolThemesModel.class);
+
+    /** The Resolver. */
+    @Self
+    private SlingHttpServletRequest request;
+
+    @Inject
+    @Optional
+    @Via("resource")
+    private String[] schoolThemes;
+
+    /*
+     * Initializing variable to be used
+     */
+    private List<SchoolThemesModelBean> schoolThemesList = null;
+
+    /**
+     * The init method
+     */
+    @PostConstruct
+    protected void init() {
+        logger.info("School Themes are  : {}" + schoolThemes);
+        schoolThemesProps();
+    }
+
+    /**
+     * Reads the authored footer links properties and adds to bean
+     *
+     */
+    private void schoolThemesProps() {
+        if (null != schoolThemes && ArrayUtils.isNotEmpty(schoolThemes)) {
+            this.schoolThemesList = new ArrayList<>();
+            for (String themesJsonString : schoolThemes) {
+                Gson gson = new Gson();
+                SchoolThemesModelBean tmb = gson.fromJson(themesJsonString, SchoolThemesModelBean.class);
+                logger.info("School Themes Model bean is : {}" + tmb);
+                schoolThemesList.add(tmb);
+            }
+        }
+    }
+
+    /**
+     * @return Map<String, List<FooterModelLinksBean>>
+     */
+    public List<SchoolThemesModelBean> getSchoolThemesList() {
+        logger.info("School Themes Links Map being sent is: : {}", schoolThemesList);
+        return schoolThemesList;
+    }
+}
---
+++ core/src/main/java/com/taylorsuniversity/models/bean/SchoolThemesModelBean.java
@@ -0,0 +1,35 @@
+/* Copyright Taylors University */
+
+package com.taylorsuniversity.models.bean;
+import java.util.List;
+
+/**
+ *
+ * This bean class is used to set and retrieve the social links and title properties
+ * configured in the footer component of the home-page template.
+ *
+ */
+public final class SchoolThemesModelBean {
+
+    private String image;
+    private String imageAlignment;
+    private String themeTitle;
+    private String themeText;
+    private List<SchoolLinksBean> schoolLinks;
+
+    public String getImage() {
+        return image;
+    }
+    public String getImageAlignment() {
+        return imageAlignment;
+    }
+    public String getThemeTitle() {
+        return themeTitle;
+    }
+    public String getThemeText() {
+        return themeText;
+    }
+    public List<SchoolLinksBean> getSchoolLinks() {
+        return schoolLinks;
+    }
+}
