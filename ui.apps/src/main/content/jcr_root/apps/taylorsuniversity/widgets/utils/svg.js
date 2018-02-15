499b36f8d8a620b348f5d27b59f62473385b90d5
Iskz <>
2018-02-15 22:47:52 +0800

    Removed Other Project Folder

--- ui.apps/src/main/content/jcr_root/apps/taylorsuniversity/widgets/utils/svg.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-use(['/apps/hlb/widgets/utils/utils.js'],function(utils){
-    return {
-        html:utils.getSVG(this.pngPath,this.iconSize,this.title,this.linkURL,this.desc,this.cssClass,this.alt,this.target,this.tabindex,this.fill)
-    };
-});
