0267217125584ee2b698a9558c0b14fefcb3b082
bsinghal <bsinghal@bsinghal-WX-1.corp.adobe.com>
2018-02-17 12:24:59 +0530

    Adding the validations for the International-Application-Form

--- ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/main/js/taylorsuniversity-applicationform.js
+++ ui.apps/src/main/content/jcr_root/etc/clientlibs/taylorsuniversity/main/js/taylorsuniversity-applicationform.js
@@ -6,13 +6,13 @@ $('#malaysian-levelOfStudy').change(function() {
 
     var levelOfStudy = $('#malaysian-levelOfStudy').val();
 
-    $( "option[name='" + levelOfStudy + "']" ).show();
+    $( "option[name='malaysian-" + levelOfStudy + "']" ).show();
 
-    if($( "option[name='" + levelOfStudy + "']" ).size() > 1) {
-		$( "option[name='" + levelOfStudy + "']:first" ).attr("selected", "selected");
+    if($( "option[name='malaysian-" + levelOfStudy + "']" ).size() > 1) {
+		$( "option[name='malaysian-" + levelOfStudy + "']:first" ).attr("selected", "selected");
     } else {
-        $( "option[name='Select Programme']" ).show();
-		$( "option[name='Select Programme']" ).attr("selected", "selected");
+        $( "option[name='malaysian-Select Programme']" ).show();
+		$( "option[name='malaysian-Select Programme']" ).attr("selected", "selected");
     }
 
 });
@@ -25,12 +25,65 @@ $(window).on("load", function () {
 
     var levelOfStudy = $('#malaysian-levelOfStudy').val();
 
-    $( "option[name='" + levelOfStudy + "']" ).show();
+    $( "option[name='malaysian-" + levelOfStudy + "']" ).show();
 
-    if($( "option[name='" + levelOfStudy + "']" ).size() > 1) {
-		$( "option[name='" + levelOfStudy + "']:first" ).attr("selected", "selected");
+    if($( "option[name='malaysian-" + levelOfStudy + "']" ).size() > 1) {
+		$( "option[name='malaysian-" + levelOfStudy + "']:first" ).attr("selected", "selected");
     } else {
-        $( "option[name='Select Programme']" ).show();
-		$( "option[name='Select Programme']" ).attr("selected", "selected");
+        $( "option[name='malaysian-Select Programme']" ).show();
+		$( "option[name='malaysian-Select Programme']" ).attr("selected", "selected");
     }
+});
+
+
+
+$('#international-levelOfStudy').change(function() {
+
+    $('#international-programmes').children('option').hide();
+
+    $("#international-programmes :selected").removeAttr("selected");
+
+    var levelOfStudy = $('#international-levelOfStudy').val();
+
+    $( "option[name='international-" + levelOfStudy + "']" ).show();
+
+    if($( "option[name='international-" + levelOfStudy + "']" ).size() > 1) {
+		$( "option[name='international-" + levelOfStudy + "']:first" ).attr("selected", "selected");
+    } else {
+        $( "option[name='international-Select Programme']" ).show();
+		$( "option[name='international-Select Programme']" ).attr("selected", "selected");
+    }
+
+});
+
+
+
+$(window).on("load", function () {
+
+	$('#international-programmes').children('option').hide();
+
+    var levelOfStudy = $('#international-levelOfStudy').val();
+
+    $( "option[name='international-" + levelOfStudy + "']" ).show();
+
+    if($( "option[name='international-" + levelOfStudy + "']" ).size() > 1) {
+		$( "option[name='international-" + levelOfStudy + "']:first" ).attr("selected", "selected");
+    } else {
+        $( "option[name='international-Select Programme']" ).show();
+		$( "option[name='international-Select Programme']" ).attr("selected", "selected");
+    }
+});
+
+
+var tabContainerId = '#tab-container-2';
+$(tabContainerId + '.t-tab-container .t-tabs > .tab-item').on('click', function(e) {
+    var container = $(this).closest('.t-tab-container');
+    $('.t-tabs > .tab-item', container).removeClass('active');
+    $(this).addClass('active');
+    var tab = $(this).data('tab') || '';
+
+
+    $('.t-tab-content > .t-tab-content-item', container).removeClass('active');
+    $('.t-tab-content > .t-tab-content-item[data-tab-content="'+ tab +'"]', container).addClass('active');
+
 });
